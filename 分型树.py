ff48e2ccafa0b208485d5d4e817f718053732a95
rainbow <1607407989@qq.com>
2021-04-20 19:48:00 +0800

    Update 分型树.py

--- 分型树.py
+++ 分型树.py
@@ -1,14 +1,12 @@
 from turtle import *
 setup(3000,800,0,0)
 
-# 设置色彩模式是RGB:
+# 设置色彩模式为RGB:
 colormode(255)
- 
 lt(90)
 lv = 14
 l = 120
 s = 45
- 
 width(lv)
  
 # 初始化RGB颜色:
@@ -24,22 +22,19 @@ fd(l)
  
 def draw_tree(l, level):
     global r, g, b
-    # save the current pen width
+    # 保存当前画笔宽度
     w = width()
- 
-    # narrow the pen width
+    # 缩小笔宽
     width(w * 3.0 / 4.0)
     # set color:
     r = r + 1
     g = g + 2
     b = b + 3
     pencolor(r % 200, g % 200, b % 200)
- 
     l = 3.0 / 4.0 * l
- 
     lt(s)
     fd(l)
- 
+
     if level < lv:
         draw_tree(l, level + 1)
     bk(l)
@@ -50,12 +45,9 @@ def draw_tree(l, level):
         draw_tree(l, level + 1)
     bk(l)
     lt(s)
- 
-    # restore the previous pen width
+    # 恢复以前的笔宽
     width(w)
- 
+    
 speed("fastest")
- 
 draw_tree(l, 4)
- 
 done()
