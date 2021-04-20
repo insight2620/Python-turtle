6f6f2d24afd254730f1328a1df9e5590d50131f5
rainbow <1607407989@qq.com>
2021-04-20 19:56:28 +0800

    Update 心型.py

--- 心型.py
+++ 心型.py
@@ -1,8 +1,7 @@
 import turtle
 
 t = turtle.Turtle()
-
-def curvemove():
+def curvemove(): #曲线函数
     for i in range(200):
         t.right(1)
         t.forward(1)
@@ -12,9 +11,9 @@ t.color('red', 'pink')
 t.begin_fill()
 t.left(140)
 t.forward(111.65)
-curvemove()
+curvemove() #画曲线
 t.left(120)
-curvemove()
+curvemove() #画曲线
 t.forward(111.65)
 t.end_fill()
 turtle.done()
