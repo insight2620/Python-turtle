1cd7225e82e8d86d5fafc8f581b38a813d977492
rainbow <1607407989@qq.com>
2021-04-20 19:53:04 +0800

    Update 太极.py

--- 太极.py
+++ 太极.py
@@ -1,27 +1,25 @@
 from turtle import *
  
-circle(100,180)
-circle(200,180)
-circle(100,-180)
- 
+circle(100,180)#小半圆
+circle(200,180)#大半圆
+circle(100,-180)#月牙中半圆
+
+ #黑色填充
 fillcolor('black')
- 
 begin_fill()
- 
 circle(100,180)
 circle(200,180)
 circle(100,-180)
- 
 end_fill()
- 
+
+ #抬起画笔
 penup()
- 
 goto(0,100)
 dot(50)
 goto(0,-100)
 pencolor('white')
 dot(50)
- 
+
+ #隐藏画笔
 hideturtle()
- 
 done()
